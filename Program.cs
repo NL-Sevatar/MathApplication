62bdf84d9fc379ca0abea948f39ca9cab21c13b2
Oden <29562343+NL-Sevatar@users.noreply.github.com>
2023-09-06 16:52:52 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,32 +8,41 @@ namespace Consoleappone
     {  
         public static void Main()
         {
-
+            bool going = true;
             double result = 0;
-
-            Console.Write("Welcome to the Math Center, What is your name? ");
-            string name = Console.ReadLine()!;
+            do{        
+                Console.Write("Welcome to the Math Center, What is your name? ");
+                string name = Console.ReadLine()!;
                 
-            Console.WriteLine("Please select the tool you require " + name);
-            Console.WriteLine("1. Calc for Calculator");
-            Console.WriteLine("2. Temp for Tempurture converter.");
+                Console.WriteLine("Please select the tool you require " + name);
+                Console.WriteLine("1. Calc for Calculator");
+                Console.WriteLine("2. Temp for Tempurture converter.");
                 
-            string getTool = Console.ReadLine()!;
+                string getTool = Console.ReadLine()!;
 
-                switch (getTool)
-                    {
-                    case "1":
-                        result = Getcalc();
-                        break;
-                    }
-            Console.WriteLine($"Result: {result}");
-        
-        }
+                    switch (getTool)
+                        {
+                        case "1":
+                            result = Getcalc();
+                            break;
+                        case "2":
+                            result = Gettemp();
+                            break;
+                        }
+                Console.WriteLine($"Result: {result}");
+
+                Console.WriteLine("Do you wish to contuine? Y/N");
+            
+
+            } while(going);
 
         static double Getcalc()
         {
-          
-                Console.Write("Please enter your first number: ");
+            bool firstTime = true;
+
+            if(firstTime)
+
+               { Console.Write("Please enter your first number: ");
                 double num1 = Convert.ToDouble(Console.ReadLine());
 
                 Console.Write("Please enter your second number: ");
@@ -63,6 +72,56 @@ namespace Consoleappone
                         Console.WriteLine("Invalid Operator. Please enter a mathmatical operator.");
                         throw new InvalidOperationException("Invalid Operator");
                 }
+               }    
         }
     }
-}     
+        static double Gettemp()
+    {
+        double result;
+
+        Console.WriteLine("Which conversion will you be making?");
+        Console.WriteLine("1. Celsius to Fahrenheit");
+        Console.WriteLine("2. Fahrenheit to Celsius");
+        Console.WriteLine("3. Celsius to Kelvin");
+        Console.WriteLine("4. Fahrenheit to Kelvin");
+        Console.WriteLine("5. Kelvin to Celsius");
+        Console.WriteLine("6. Kelvin to Fahrenheit");
+
+        double converter = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Now please enter your base tempature: ");
+        double baseTemp = Convert.ToDouble(Console.ReadLine());
+
+
+        switch (converter)
+        {
+            case 1:
+                (baseTemp * 9/5) + 32; 
+                break;
+
+            case 2:
+                (baseTemp - 32) * 5 / 9;
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+
+            case 5:
+
+                break;
+
+            case 6:
+
+                break;                                        
+        }
+
+    }
+
+
+} 
+}    
