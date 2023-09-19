9156e451fd66e958b8fba39e36bd8e20cd3c1c16
Sevatar <29562343+NL-Sevatar@users.noreply.github.com>
2023-09-18 22:31:42 -0500

    Adding more function to the calculator app. Allowing for chaining of equations.

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace Consoleappone
     {  
         public static void Main()
         {
-            bool going = true;
+            bool firstTime = true;
             double result = 0;
-            do{        
+           
                 Console.Write("Welcome to the Math Center, What is your name? ");
                 string name = Console.ReadLine()!;
                 
@@ -21,49 +21,48 @@ namespace Consoleappone
                 string getTool = Console.ReadLine()!;
 
                     switch (getTool)
-                        {
+                        {   
                         case "1":
-                            result = Getcalc();
+                            if (firstTime)
+                            {
+                                result = Getcalcfirsttime();
+                                firstTime = false;
+                            }
+                            else
+                            {
+                                result = Getcalc(result);
+                            }
                             break;
-                        case "2":
+                        /* case "2":
                             result = Gettemp();
                             break;
-                        }
-                Console.WriteLine($"Result: {result}");
-
-                Console.WriteLine("Do you wish to contuine? Y/N");
-            
-
-            } while(going);
-
-        static double Getcalc()
+                        } */
+  
+        static double Getcalcfirsttime()
         {
-            bool firstTime = true;
+            
+             Console.Write("Please enter your first number: ");
+             double num1 = Convert.ToDouble(Console.ReadLine());
 
-            if(firstTime)
+            Console.Write("Please enter your second number: ");
+             double num2 = Convert.ToDouble(Console.ReadLine());
 
-               { Console.Write("Please enter your first number: ");
-                double num1 = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("Please enter your operator: ");
+             char op =  Convert.ToChar(Console.ReadLine()!);
 
-                Console.Write("Please enter your second number: ");
-                double num2 = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Please enter your operator: ");
-                char op =  Convert.ToChar(Console.ReadLine()!);
-
-                switch (op)
-                {
-                    case '+':
-                        return num1 + num2;
-                    case '-': 
-                        return num1 - num2;
-                    case '/': 
-                        if (num2 != 0) {
+             switch (op)
+              {
+                 case '+':
+                     return num1 + num2;
+                 case '-': 
+                     return num1 - num2;
+                 case '/': 
+                     if (num2 != 0) {
                         return num1 / num2;
-                        }
-                        else{
-                          throw new DivideByZeroException("Divison by zero");
-                        }
+                    }
+                   else{
+                        throw new DivideByZeroException("Divison by zero");
+                    }
                     case '*': 
                         return num1 * num2;
                     case '%': 
@@ -72,56 +71,46 @@ namespace Consoleappone
                         Console.WriteLine("Invalid Operator. Please enter a mathmatical operator.");
                         throw new InvalidOperationException("Invalid Operator");
                 }
-               }    
-        }
-    }
-        static double Gettemp()
-    {
-        double result;
+               }  
 
-        Console.WriteLine("Which conversion will you be making?");
-        Console.WriteLine("1. Celsius to Fahrenheit");
-        Console.WriteLine("2. Fahrenheit to Celsius");
-        Console.WriteLine("3. Celsius to Kelvin");
-        Console.WriteLine("4. Fahrenheit to Kelvin");
-        Console.WriteLine("5. Kelvin to Celsius");
-        Console.WriteLine("6. Kelvin to Fahrenheit");
-
-        double converter = Convert.ToDouble(Console.ReadLine());
-
-        Console.Write("Now please enter your base tempature: ");
-        double baseTemp = Convert.ToDouble(Console.ReadLine());
-
-
-        switch (converter)
+        static double Getcalc(double num1)
         {
-            case 1:
-                (baseTemp * 9/5) + 32; 
-                break;
+            Console.WriteLine($"Using previous result {num1} as num1.");
 
-            case 2:
-                (baseTemp - 32) * 5 / 9;
-                break;
+            Console.Write("Please enter your second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
 
-            case 3:
+            Console.WriteLine("Please enter your operator: ");
+            char op = Convert.ToChar(Console.ReadLine()!);
 
-                break;
+            switch (op)
+            {
+                case '+':
+                    return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        return num1 / num2;
+                    }
+                    else
+                    {
+                        throw new DivideByZeroException("Division by zero");
+                    }
+                case '*':
+                    return num1 * num2;
+                case '%':
+                    return num1 % num2;
+                default:
+                    Console.WriteLine("Invalid Operator. Please enter a mathematical operator.");
+                    throw new InvalidOperationException("Invalid Operator");
+            }
+        }       
 
-            case 4:
-
-                break;
-
-            case 5:
-
-                break;
-
-            case 6:
-
-                break;                                        
-        }
-
-    }
-
+        
+}
 
 } 
-}    
+}
+}
---
+++ Temp.cs.fs
@@ -0,0 +1,47 @@
+/* static double Gettemp()
+    {
+        double result;
+
+        Console.WriteLine("Which conversion will you be making?");
+        Console.WriteLine("1. Celsius to Fahrenheit");
+        Console.WriteLine("2. Fahrenheit to Celsius");
+        Console.WriteLine("3. Celsius to Kelvin");
+        Console.WriteLine("4. Fahrenheit to Kelvin");
+        Console.WriteLine("5. Kelvin to Celsius");
+        Console.WriteLine("6. Kelvin to Fahrenheit");
+
+        double converter = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Now please enter your base tempature: ");
+        double baseTemp = Convert.ToDouble(Console.ReadLine());
+
+
+        switch (converter)
+        {
+            case 1:
+                (baseTemp * 9/5) + 32; 
+                break;
+
+            case 2:
+                (baseTemp - 32) * 5 / 9;
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+
+            case 5:
+
+                break;
+
+            case 6:
+
+                break;                                        
+        }
+
+    } */
+
