ffa086929117e098ee2054e7dd504a6c82dc7431
Oden <29562343+NL-Sevatar@users.noreply.github.com>
2023-09-20 17:07:53 -0500

    Added some looping functionality for the calculator with some changes to order of info gathered

--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections;
-using System.Xml.XPath;
-
-namespace Consoleappone
+﻿namespace MathApplication
 {
     class Program
     {  
         public static void Main()
         {
+            bool contuineCalculation = true;
             bool firstTime = true;
             double result = 0;
            
@@ -15,40 +12,100 @@ namespace Consoleappone
                 string name = Console.ReadLine()!;
                 
                 Console.WriteLine("Please select the tool you require " + name);
-                Console.WriteLine("1. Calc for Calculator");
-                Console.WriteLine("2. Temp for Tempurture converter.");
+                Console.WriteLine("1. Calculator");
+                Console.WriteLine("2. Tempurture Converter.");
+                Console.WriteLine("3. Currency Converter");
+                Console.WriteLine("4. Exit");
                 
                 string getTool = Console.ReadLine()!;
 
                     switch (getTool)
                         {   
                         case "1":
-                            if (firstTime)
-                            {
-                                result = Getcalcfirsttime();
-                                firstTime = false;
+                            do {
+                                if (firstTime)
+                                {
+                                    result = Getcalcfirsttime();
+                                    firstTime = false;
+                                }
+                                else if (!firstTime)
+                                {
+                                    result = Getcalc(result);
+                                }
+                                else {
+                                    Console.WriteLine("Error Detected. Please try again.");
+                                    }
                             }
-                            else
-                            {
-                                result = Getcalc(result);
-                            }
+                            while(contuineCalculation);
                             break;
-                        /* case "2":
+                     /* case "2":
                             result = Gettemp();
                             break;
                         } */
-  
-        static double Getcalcfirsttime()
+                        case "3":
+
+                            break;
+                        case "4":
+                            Console.WriteLine($"Goodbye {name}.");
+                            break;    
+                        default:
+                            Console.WriteLine("Invalid option, please try using the numbers.");
+                            break; 
+                        }
+
+                Console.WriteLine($"Result: {result}");
+                Main();
+        }
+
+        public static double Getcalc(double num1)
+        {
+            Console.WriteLine($"Using previous result {num1}.");
+
+            Console.WriteLine("Please enter your operator or q to quit: ");
+            char op =  Convert.ToChar(Console.ReadLine()!);
+
+            Console.Write("Please enter your second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+         
+            switch (op)
+            {
+                case '+':
+                    return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        return num1 / num2;
+                    }
+                    else
+                    {
+                        throw new DivideByZeroException("Division by zero");
+                    }
+                case '*':
+                    return num1 * num2;
+                case '%':
+                    return num1 % num2;
+               /* case 'q':
+                    Console.WriteLine("Exiting Calculator"); Need to build a exit out to main menu that changes contuineCalculation to false.
+                    return contuineCalculation = false; */
+                default:
+                    Console.WriteLine("Invalid Operator. Please enter a mathematical operator.");
+                    throw new InvalidOperationException("Invalid Operator");
+            }
+        }       
+
+       static double Getcalcfirsttime()
         {
             
-             Console.Write("Please enter your first number: ");
+            Console.Write("Please enter your first number: ");
              double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter your operator: ");
+             char op =  Convert.ToChar(Console.ReadLine()!);
 
             Console.Write("Please enter your second number: ");
              double num2 = Convert.ToDouble(Console.ReadLine());
-
-             Console.WriteLine("Please enter your operator: ");
-             char op =  Convert.ToChar(Console.ReadLine()!);
 
              switch (op)
               {
@@ -73,44 +130,8 @@ namespace Consoleappone
                 }
                }  
 
-        static double Getcalc(double num1)
-        {
-            Console.WriteLine($"Using previous result {num1} as num1.");
-
-            Console.Write("Please enter your second number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Please enter your operator: ");
-            char op = Convert.ToChar(Console.ReadLine()!);
-
-            switch (op)
-            {
-                case '+':
-                    return num1 + num2;
-                case '-':
-                    return num1 - num2;
-                case '/':
-                    if (num2 != 0)
-                    {
-                        return num1 / num2;
-                    }
-                    else
-                    {
-                        throw new DivideByZeroException("Division by zero");
-                    }
-                case '*':
-                    return num1 * num2;
-                case '%':
-                    return num1 % num2;
-                default:
-                    Console.WriteLine("Invalid Operator. Please enter a mathematical operator.");
-                    throw new InvalidOperationException("Invalid Operator");
-            }
-        }       
 
         
 }
 
 } 
-}
-}
