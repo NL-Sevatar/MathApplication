6e297955dde97cbf7f1b99d4e34f0e2bfcbf29ba
Oden <29562343+NL-Sevatar@users.noreply.github.com>
2023-09-22 16:49:59 -0500

    Updated logic in the Calculator to exit the loop more gracefully and incorprated the Temp conversion method to the application.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MathApplication
+﻿using System.Xml.XPath;
+
+namespace MathApplication
 {
     class Program
     {  
@@ -6,12 +8,12 @@
         {
             bool contuineCalculation = true;
             bool firstTime = true;
-            double result = 0;
+            double result =0;
+            string tempResult = " ";
            
-                Console.Write("Welcome to the Math Center, What is your name? ");
-                string name = Console.ReadLine()!;
+                Console.WriteLine("Welcome to the Math Center");
                 
-                Console.WriteLine("Please select the tool you require " + name);
+                Console.WriteLine("Please select the tool you require ");
                 Console.WriteLine("1. Calculator");
                 Console.WriteLine("2. Tempurture Converter.");
                 Console.WriteLine("3. Currency Converter");
@@ -38,32 +40,35 @@
                             }
                             while(contuineCalculation);
                             break;
-                     /* case "2":
-                            result = Gettemp();
+                         case "2":
+                            tempResult = GetTemp();
+                            Console.WriteLine($"The temp conversion is {tempResult}");
+                            Console.WriteLine("**************************");
+                            Main();
                             break;
-                        } */
-                        case "3":
-
-                            break;
+                        
+                         case "3":
+                            GetCurrency();
+                            break; 
                         case "4":
-                            Console.WriteLine($"Goodbye {name}.");
+                            Console.WriteLine("Thank you for visting.");
                             break;    
                         default:
                             Console.WriteLine("Invalid option, please try using the numbers.");
                             break; 
                         }
-
-                Console.WriteLine($"Result: {result}");
-                Main();
         }
 
         public static double Getcalc(double num1)
         {
-            Console.WriteLine($"Using previous result {num1}.");
+            Console.WriteLine($"Result: {num1}");
 
             Console.WriteLine("Please enter your operator or q to quit: ");
             char op =  Convert.ToChar(Console.ReadLine()!);
-
+            if (op == 'q'){
+                Main();
+            }
+                
             Console.Write("Please enter your second number: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
          
@@ -86,9 +91,6 @@
                     return num1 * num2;
                 case '%':
                     return num1 % num2;
-               /* case 'q':
-                    Console.WriteLine("Exiting Calculator"); Need to build a exit out to main menu that changes contuineCalculation to false.
-                    return contuineCalculation = false; */
                 default:
                     Console.WriteLine("Invalid Operator. Please enter a mathematical operator.");
                     throw new InvalidOperationException("Invalid Operator");
@@ -128,7 +130,67 @@
                         Console.WriteLine("Invalid Operator. Please enter a mathmatical operator.");
                         throw new InvalidOperationException("Invalid Operator");
                 }
-               }  
+        }  
+        static string GetTemp()
+     {
+            Console.WriteLine("Which conversion will you be making?");
+            Console.WriteLine("1. Celsius to Fahrenheit");
+            Console.WriteLine("2. Fahrenheit to Celsius");
+            Console.WriteLine("3. Celsius to Kelvin");
+            Console.WriteLine("4. Fahrenheit to Kelvin");
+            Console.WriteLine("5. Kelvin to Celsius");
+            Console.WriteLine("6. Kelvin to Fahrenheit");
+
+            double converter = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Now please enter your base tempature: ");
+            double baseTemp = Convert.ToDouble(Console.ReadLine());
+            double outTemp = 0;
+            string tempType = " ";
+
+
+            switch (converter)
+            {
+                case 1:
+                    outTemp =(baseTemp * 9/5) + 32; 
+                    tempType = "Fahrenheit";
+                    break;
+
+                case 2:
+                    outTemp = (baseTemp - 32) * 5 / 9;
+                    tempType = "Celsius";
+                    break;
+
+                case 3:
+                    outTemp = baseTemp + 273.15;
+                    tempType = "Kelvin";
+                    break;
+
+                case 4:
+                    outTemp = (baseTemp - 32) * 5/9 + 273.15;
+                    tempType = "Kelvin";
+                    break;
+
+                case 5:
+                    outTemp = baseTemp - 273.15;
+                    tempType = "Celsius";
+                    break;
+
+                case 6:
+                    outTemp = (baseTemp - 273.15) * 9 / 5 + 32;
+                    tempType = "Fahrenheit";
+                    break;                                        
+            }
+
+        return $"{outTemp} {tempType}";
+
+    } 
+
+    static void GetCurrency()
+    {
+
+    }
+
 
 
         
